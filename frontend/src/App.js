ed7a46e92294e6ff8f61648bb938f448c395f9a7
unknown <melkywafula29@gmail.com>
2025-06-02 22:23:05 +0300

    more updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,15 +5,19 @@ import Navbar from './components/Navbar';
 import Login from './components/Login';
 import VerifyCode from './components/VerifyCode';
 import Plans from './components/Plans';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 const App = () => {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/verify" element={<VerifyCode />} />
         <Route path="/plans" element={<Plans />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
