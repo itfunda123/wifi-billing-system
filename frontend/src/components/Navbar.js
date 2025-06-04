6cbf806c9be8c48e5fea1f02b250551d1e8ac350
unknown <melkywafula29@gmail.com>
2025-06-04 10:55:38 +0300

    more updates

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -6,10 +6,19 @@ const Navbar = () => (
   <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
     <div className="container">
       <Link className="navbar-brand" to="/">WiFi Billing</Link>
-      <div>
-        <Link className="nav-link d-inline text-white" to="/">Home</Link>
-        <Link className="nav-link d-inline text-white" to="/login">Login</Link>
-        <Link className="nav-link d-inline text-white" to="/plans">Plans</Link>
+      
+      <div className="collapse navbar-collapse">
+        <ul className="navbar-nav ms-auto">
+          <li className="nav-item">
+            <Link className="nav-link text-white" to="/">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link text-white" to="/login">Login</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link text-white" to="/plans">Plans</Link>
+          </li>
+        </ul>
       </div>
     </div>
   </nav>
