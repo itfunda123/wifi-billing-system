cd8b3840f1b513767ff9bb9e50fd99931cf038a1
unknown <melkywafula29@gmail.com>
2025-06-04 10:57:45 +0300

    added react icons

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,7 @@
 // src/components/Navbar.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaHome, FaSignInAlt, FaBoxOpen } from 'react-icons/fa'; // import icons
 
 const Navbar = () => (
   <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -10,13 +11,19 @@ const Navbar = () => (
       <div className="collapse navbar-collapse">
         <ul className="navbar-nav ms-auto">
           <li className="nav-item">
-            <Link className="nav-link text-white" to="/">Home</Link>
+            <Link className="nav-link text-white" to="/">
+              <FaHome className="me-1" /> Home
+            </Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link text-white" to="/login">Login</Link>
+            <Link className="nav-link text-white" to="/login">
+              <FaSignInAlt className="me-1" /> Login
+            </Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link text-white" to="/plans">Plans</Link>
+            <Link className="nav-link text-white" to="/plans">
+              <FaBoxOpen className="me-1" /> Plans
+            </Link>
           </li>
         </ul>
       </div>
