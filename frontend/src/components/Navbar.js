44f5c11378016d84ea11f675e69e4b1b63a03641
unknown <melkywafula29@gmail.com>
2025-06-06 08:43:39 +0300

    added responsiveness

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,14 +1,27 @@
-// src/components/Navbar.js
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FaHome, FaSignInAlt, FaBoxOpen } from 'react-icons/fa'; // import icons
+import { FaHome, FaSignInAlt, FaBoxOpen } from 'react-icons/fa';
 
 const Navbar = () => (
   <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
     <div className="container">
       <Link className="navbar-brand" to="/">WiFi Billing System</Link>
-      
-      <div className="collapse navbar-collapse">
+
+      {/* Toggle button for mobile view */}
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      {/* Collapsible menu */}
+      <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav ms-auto">
           <li className="nav-item">
             <Link className="nav-link text-white" to="/">
